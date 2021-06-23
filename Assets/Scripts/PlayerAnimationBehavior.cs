5e83d72fddf916a67cbf7e4e71430642d7fad888
Bryce <brycedeso123@gmail.com>
2021-06-23 16:10:06 -0500

    Did exercise

---
+++ Assets/Scripts/PlayerAnimationBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehavior : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    private PlayerMovementBehaviour _movementBehavior;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _movementBehavior = GetComponent<PlayerMovementBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Speed", _movementBehavior.Velocity.magnitude);
+        _animator.SetBool("InAir", _movementBehavior.IsGrounded);
+    }
+
+    public void SetWinTrigger()
+    {
+        _animator.SetTrigger("WonGame");
+    }
+}
